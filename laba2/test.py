8d116311965c8aba3288837f2f3ee0afbfafcf0a
Sofi <117453504+SofyaDemeneva@users.noreply.github.com>
2024-10-06 02:33:40 +0500

    Update test.py

--- laba2/test.py
+++ laba2/test.py
@@ -1,54 +1,32 @@
+import pytest
 from lib import fibonacci, bubble_sort, calculator
-def tcalculator():
-    try:
-        assert calculator(3, 2, '+') == 5
-        assert calculator(3, 2, '-') == 1
-        assert calculator(3, 2, '*') == 6
-        assert calculator(6, 2, '/') == 3
-        print("Тест для функции calculator пройден")
-    except AssertionError:
-        print("Тест для функции calculator не пройден")
 
-    try:
-        calculator(3, 0, '/')  # Этот тест должен выбросить исключение
-    except ValueError:
-        print("Тест для функции calculator (деление на ноль) пройден")
-    except Exception:
-        print("Тест для функции calculator (деление на ноль) не пройден")
-
-    try:
-        calculator(3, 2, 'i')  # Этот тест должен выбросить исключение
-    except ValueError:
-        print("Тест для функции calculator (неверная операция) пройден")
-    except Exception:
-        print("Тест для функции calculator (неверная операция) не пройден")
+# Тест для функции calculator
+def test_calculator():
+    assert calculator(3, 2, '+') == 5
+    assert calculator(3, 2, '-') == 1
+    assert calculator(3, 2, '*') == 6
+    assert calculator(6, 2, '/') == 3
+    
+    with pytest.raises(ValueError):
+        calculator(3, 0, '/')  # Должно выбросить исключение деления на ноль
+    
+    with pytest.raises(ValueError):
+        calculator(3, 2, 'i')  # Должно выбросить исключение неверной операции
 
 
-def tfibonacci():
-    try:
-        assert fibonacci(1) == [0]
-        assert fibonacci(2) == [0, 1]
-        assert fibonacci(5) == [0, 1, 1, 2, 3]
-        print("Тест для функции fibonacci пройден")
-    except AssertionError:
-        print("Тест для функции fibonacci не пройден")
-    try:
-        fibonacci(0)  # Этот тест должен выбросить исключение
-    except ValueError:
-        print("Тест для функции fibonacci (ошибочный ввод) пройден")
-    except Exception:
-        print("Тест для функции fibonacci (ошибочный ввод) не пройден")
+# Тест для функции fibonacci
+def test_fibonacci():
+    assert fibonacci(1) == [0]
+    assert fibonacci(2) == [0, 1]
+    assert fibonacci(5) == [0, 1, 1, 2, 3]
+
+    with pytest.raises(ValueError):
+        fibonacci(0)  # Должно выбросить исключение для неверного ввода
 
 
-def tbubble_sort():
-    try:
-        assert bubble_sort([4, 2, 7, 1]) == [1, 2, 4, 7]
-        assert bubble_sort([]) == []
-        assert bubble_sort([3]) == [3]
-        print("Тест для функции bubble_sort пройден")
-    except AssertionError:
-        print("Тест для функции bubble_sort не пройден")
-if __name__ == "__main__":
-    tfibonacci()
-    tbubble_sort()
-    tcalculator()
+# Тест для функции bubble_sort
+def test_bubble_sort():
+    assert bubble_sort([4, 2, 7, 1]) == [1, 2, 4, 7]
+    assert bubble_sort([]) == []
+    assert bubble_sort([3]) == [3]
